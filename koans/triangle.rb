f0720e55ec169759f786bab4172ba62dca3862ad
Ariel May <aemd199711@gmail.com>
2021-01-04 19:58:15 -0600

    feat: about_triangle_project_2 illegal triangles throw exceptions

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,6 +15,10 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  raise TriangleError if [a,b,c].min <= 0
+  x, y, z = [a,b,c].sort
+  raise TriangleError if x + y <= z
+  
   return :equilateral if a==b && b==c
   return :isosceles if a==b || b==c || c==a
   return :scalene if a!=b && b!=c && a!=c
