f4f2f3ecbf2b0fdda0e031e8aacf7bbd79053e07
Ariel May <aemd199711@gmail.com>
2021-01-19 21:39:55 -0600

    feat: about_proxy_object_project created methods to implement a proxy

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -15,9 +15,34 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class Proxy
   def initialize(target_object)
     @object = target_object
+    @messages = Hash.new
     # ADD MORE CODE HERE
   end
 
+  def called?(method_name)
+    @messages.keys.include? method_name
+  end
+
+  def messages
+    @messages.keys
+  end
+
+  def number_of_times_called(method_name)
+    @messages[method_name] ? @messages[method_name] : 0
+  end
+
+  def method_missing(method_name, *args, &block)
+    if @object.respond_to? method_name
+      if(@messages.keys.include? method_name)
+        @messages[method_name] += 1
+      else
+        @messages[method_name] = 1
+      end
+      @object.send(method_name, *args)
+    else
+      super(method_name, *args, &block)
+    end
+  end
   # WRITE CODE HERE
 end
 
